29af27c8f26c9c507cb7009bddaedbec7a3da931
Persiano99 <sorenrahimi99@gmail.com>
2024-05-10 00:08:09 +0200

    Set up Material, Creation of Signup, API Call and Auth Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { DemoAngularMaterialModule } from './DemoAngularMaterialModule';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, ReactiveFormsModule,
+    DemoAngularMaterialModule, FormsModule, ReactiveFormsModule, HttpClientModule, CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+closeMenu() {
+throw new Error('Method not implemented.');
+}
+isMenuOpen: any;
+toggleMenu() {
+throw new Error('Method not implemented.');
+}
   title = 'cp';
 }
+
